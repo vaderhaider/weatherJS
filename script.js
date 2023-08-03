79e16d8bc4245878ab4d34a8d828aab94f0432a8
vaderhaider <vaderhaider@gmail.com>
2023-08-03 13:43:04 -0400

    Search city functionality working

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const apiKey = "828b0e718bd0ea07d7f49fece2035c37"
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=Toronto";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 
-async function checkWeather(){
-    const response = await fetch(apiUrl + `&appid=${apiKey}`)
+const searchbox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`)
     var data = await response.json()
 
     console.log(data)
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = math.round(data.main.temp) +"°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity +"%";
+    document.querySelector(".wind").innerHTML = data.wind.speed +" km/h";
+
+
+
 }
 
-checkWeather()
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchbox.value);
+})
